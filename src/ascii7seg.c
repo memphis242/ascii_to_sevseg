ad6c5722f3f4bc3da2e88d84bf65af3b438c62d1
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-05-18 19:29:02 -0500

    :building_construction: Blank src file

---
+++ src/ascii7seg.c
@@ -0,0 +1,38 @@
+/**
+ * @file ascii7seg.c
+ * @brief Implementation of the ASCII to 7 Segment encoding API.
+ *
+ * @author Abdulla Almosalami (memphis242)
+ * @date May 18, 2025
+ * @copyright MIT License
+ */
+
+/* File Inclusions */
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+#include "ascii7seg.h"
+
+/* Local Macro Definitions */
+
+// Constant-like macros
+
+// Function-like macros
+
+/* Local Datatypes */
+
+/* Private Function Prototypes */
+
+/* Public API Implementations */
+
+bool Ascii7Seg_ConvertChar( char ascii_char, struct Ascii7Seg_Encoding_S * buf )
+{
+
+}
+
+bool Ascii7Seg_ConvertWord( const char * str,
+                            size_t str_len,
+                            struct Ascii7Seg_Encoding_S * buf )
+{
+
+}
