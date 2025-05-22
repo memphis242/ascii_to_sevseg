23656e05331988af6d2198c94ac6beee4abb08d9
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-05-21 23:27:23 -0500

    :building_construction: Implemented `Ascii7Seg_ConvertWord`

--- src/ascii7seg.c
+++ src/ascii7seg.c
@@ -58,5 +58,28 @@ bool Ascii7Seg_ConvertWord( const char * str,
                             size_t str_len,
                             union Ascii7Seg_Encoding_U * buf )
 {
+   if ( (NULL == str) || (NULL == buf) )
+   {
+      return false;
+   }
 
+#ifndef ASCII_7SEG_MAX_WORD_LEN
+#define ASCII_7SEG_MAX_WORD_LEN 100
+#endif
+   bool word_converted = false;
+   unsigned int iterations = 0;
+   while ( (str[iterations] != '\0') && (iterations < ASCII_7SEG_MAX_WORD_LEN) )
+   {
+      word_converted = Ascii7Seg_ConvertChar( str[iterations],
+                                              &buf[iterations] );
+      if ( !word_converted )
+      {
+         // Character failed to convert
+         break;
+      }
+
+      iterations++;
+   }
+
+   return word_converted;
 }
